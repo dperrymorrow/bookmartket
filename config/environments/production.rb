d05923557288cffe6af1fbf4f255ad820374f120
David Morrow <david.morrow@collectiveintellect.com>
2012-04-05 15:12:50 -0600

    precompile manifest

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,12 @@ Booksmartlet::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( iframe.js )
+  config.assets.precompile += %w( site.js )
+  config.assets.precompile += %w( boostrap.min.js )
+  config.assets.precompile += %w( site.css )
+  config.assets.precompile += %w( iframe_app.css )
+
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
